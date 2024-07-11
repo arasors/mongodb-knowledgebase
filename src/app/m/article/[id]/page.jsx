d395d50ce2f500ff50b18618fb837b47f2ce75ea
Ateş <ates@betandworks.com>
2024-07-11 14:24:41 +0300

    rich text editor

--- src/app/m/article/[id]/page.jsx
+++ src/app/m/article/[id]/page.jsx
@@ -1,23 +1,60 @@
 "use client";
-import React, {memo} from 'react';
+import React, {memo, useState} from 'react';
 import {useParams} from "next/navigation";
-import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
-
+import {
+    EditorComposer,
+    Editor,
+    ToolbarPlugin,
+    AlignDropdown,
+    BackgroundColorPicker,
+    BoldButton,
+    CodeFormatButton,
+    FloatingLinkEditor,
+    FontFamilyDropdown,
+    FontSizeDropdown,
+    InsertDropdown,
+    InsertLinkButton,
+    ItalicButton,
+    TextColorPicker,
+    TextFormatDropdown,
+    UnderlineButton,
+    Divider,
+} from 'verbum';
 function Page() {
     const params = useParams();
+    const [state, setState] = useState(`<b>aras</b><i>test metni</i>`);
 
     return (
-        <div className="container mx-auto my-8 flex flex-wrap justify-center">
+        <div className="container mx-auto my-8 flex flex-col flex-wrap justify-center">
             <h3 className={"text-lg font-medium"}>{params?.id === "new" ? "Yeni makale" : "Makaleyi düzenle"}</h3>
 
-            <RichTextEditorComponent
-                quickToolbarSettings={{
-                    text: ['Bold', 'Italic', 'Underline', 'FontColor', 'BackgroundColor', 'Alignments', '-', 'FontSize', 'FontName', 'Formats', 'OrderedList', 'UnorderedList', 'FormatPainter']
-                }}
-            >
-                <p>The Rich Text Editor component is WYSIWYG ("what you see is what you get") editor that provides the best user experience to create and update the content. Users can format their content using standard toolbar commands.</p>
-                <Inject services={[Toolbar, Image, Link, HtmlEditor, QuickToolbar]} />
-            </RichTextEditorComponent>
+            <EditorComposer>
+                <Editor
+                    hashtagsEnabled={true}
+                    onChange={(val) => setState(val)}
+                    autoLinkEnabled={true}
+                    emojisEnabled={true}
+                    actionsEnabled={true}
+                >
+                    <ToolbarPlugin defaultFontSize="20px" style={{height: "50px"}}>
+                        <FontFamilyDropdown />
+                        <FontSizeDropdown />
+                        <Divider />
+                        <BoldButton />
+                        <ItalicButton />
+                        <UnderlineButton />
+                        <CodeFormatButton />
+                        <InsertLinkButton />
+                        <TextColorPicker />
+                        <BackgroundColorPicker />
+                        <TextFormatDropdown />
+                        <Divider />
+                        <InsertDropdown enablePoll={true} />
+                        <Divider />
+                        <AlignDropdown />
+                    </ToolbarPlugin>
+                </Editor>
+            </EditorComposer>
 
         </div>
     );
